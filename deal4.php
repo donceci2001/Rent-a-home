0ccb6884b785f8f0817ac54b4300c679e4e06042
donceci2001 <ceckopavlov@gmail.com>
2022-07-07 12:05:55 +0300

    Rent a home

---
+++ deal4.php
@@ -0,0 +1,45 @@
+
+<?php
+require_once "functions.php";
+require_once "db.php";
+
+$con = mysqli_connect('localhost', 'root', '', 'project');
+
+$query = "SELECT * FROM deal WHERE id = '4'";
+$result = mysqli_query($con, $query);
+$row =mysqli_fetch_assoc($result);
+
+?>
+<!DOCTYPE html>
+    <html lang="en" >
+      <head>        
+             <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+             <link rel="stylesheet" href="styleforlogin.css">
+        </head>
+        <body>
+                <div class="login-div">
+                <div class="title">Benkovski-45000$</div>
+                     <div class="img">
+                        <img src="images/b-4.jpg" alt="">
+                      </div>
+                     <div class = "desc">
+                         <p>
+                         <?php echo $row['text']?>
+                         </p>
+                    </div>   
+                     <div class="sign-up">
+                     <input type="button" name= "send" onclick= "location.href='index.php'" value="Home page" />
+                    </div>
+                    <div class="password">
+                        <input type = "text" placeholder="Offer" name="offer" >
+                    </div>
+                        
+                     <div class ="sign-in">
+                       <button type="submit" class="signin-btn" value="Login" name="btn-login">Submit</button>
+                    </div>
+                </div>
+
+        </body>
+      
+        
+</html>
